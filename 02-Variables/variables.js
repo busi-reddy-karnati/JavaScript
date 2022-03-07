a758c3f0b74c685e062e3ab3a2896948f4d3387e
busisatya@gmail.com <busisatya@gmail.com>
2022-03-07 12:57:04 -0500

    Init Setup

---
+++ 02-Variables/variables.js
@@ -0,0 +1,15 @@
+var first_name = "Busi Reddy";
+var last_name = "Karnati";
+
+var val = 100
+
+//Strings can be added with integers and strings can be added together ofcourse
+console.log(first_name+last_name+val)
+
+var val = 3.14;
+console.log(val*2)
+
+var d;//d here is undefined and we can have a JS variable un-initialized
+console.log(d)
+d = 100
+console.log(d)

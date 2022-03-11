8bff9866b998f7046303b7b66d786039836c26aa
busisatya@gmail.com <busisatya@gmail.com>
2022-03-11 17:02:27 -0500

    Operators and related

---
+++ 02-Variables/Operators.js
@@ -0,0 +1,26 @@
+var num1 = 10;
+var num2 = 13;
+console.log(num1+num2);
+
+var s = "hello";
+var t = "world";
+var combined = s + " " + t;
+console.log(combined);
+
+var n1 = 10;
+var n2 = 10;
+var n3 = "10";
+
+//This checks is the values are equal(ignores the type)
+console.log(n1 == n3);
+//Checks the type too.
+console.log(n1 === n3);
+console.log(typeof (n1 === n3));
+
+// b = 5 returns 5 to a and a = 5 returns 5 so prints 5
+console.log(a = b = 5)
+
+
+percent = 74.9989878988272738;
+console.log(typeof percent);
+console.log(Math.round(percent));

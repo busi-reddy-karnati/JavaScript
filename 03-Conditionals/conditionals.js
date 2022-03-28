294810053c350fce47b0c355520e806f9f8390a6
busisatya@gmail.com <busisatya@gmail.com>
2022-03-27 20:18:03 -0400

    conditionals, ternary

---
+++ 03-Conditionals/conditionals.js
@@ -0,0 +1,30 @@
+var temperature;
+
+temperature = 21;
+
+if(temperature == 20){
+    console.log("The temp is 20");
+}
+else{
+    console.log("Not 20");
+}
+
+// {
+// } This is called a block of code
+temperature = 10;
+if(temperature < 20){
+    console.log("0-20");
+}
+else if(temperature < 30){
+    console.log("20-30");
+}
+else{
+    console.log("30+");
+}
+
+//or, and, are same as C
+
+//Ternary operator
+boool = true
+val = boool ? 2:3;
+console.log("val :",val)
